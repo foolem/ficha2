8c7041b2d373dabcf82f3a5b65c6532c25577d3e
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2017-08-16 10:50:26 -0300

    Icons update

---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,95 @@
+module IconsHelper
+
+  def remove_button
+    '<i class="glyphicon glyphicon-remove"></i>'
+  end
+
+  def enable_button
+    '<i class="glyphicon glyphicon-off"></i>'
+  end
+
+  def desable_button
+    '<i class="glyphicon glyphicon-off"></i>'
+  end
+
+  def edit_button
+    '<i class="glyphicon glyphicon-edit"></i> Editar'
+  end
+
+  def new_button
+    '<i class="glyphicon glyphicon-plus"></i>'
+  end
+
+  def download_button
+    '<i class="glyphicon glyphicon-save"></i> Baixar'
+  end
+
+  def show_button
+    '<i class="glyphicon glyphicon-eye-open"></i>'
+  end
+
+  def duplicate_button
+    '<i class="glyphicon glyphicon-duplicate"></i>'
+  end
+
+  def search_button
+    '<i class="glyphicon glyphicon-search"></i>'
+  end
+
+  def back_button
+    '<i class="glyphicon glyphicon-arrow-left"></i>'
+  end
+
+  def config_button
+    '<i class="glyphicon glyphicon-cog"></i>'
+  end
+
+  def reload_button
+    '<i style="color: greeen" class="glyphicon glyphicon-refresh"></i> Atualizar'
+  end
+
+  def help_button
+    '<i  class="glyphicon glyphicon-pushpin"></i>'
+  end
+
+  def user_button(user)
+    '<i class="glyphicon glyphicon-user"></i> ' + user
+  end
+
+  def home_icon
+    'home'
+  end
+
+  def ficha_icon
+    'list-alt'
+  end
+
+  def matter_icon
+    'tags'
+  end
+
+  def group_icon
+    'th'
+  end
+
+  def user_icon
+    'user'
+  end
+
+  def help_icon
+    'envelope'
+  end
+
+  def account_icon
+    'cog'
+  end
+
+  def menu_icon(key)
+    "pull-right hidden-sm showopacity menu_font_size glyphicon glyphicon-#{key}"
+  end
+
+  def index_icon(key)
+    "glyphicon glyphicon-#{key} blue"
+  end
+
+ end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,40 @@ module UsersHelper
     @teachers = User.all.where("role != 3").where(actived: true).order(name: :asc)
   end
 
+  def user_teacher?
+    user_signed_in? && current_user.teacher?
+  end
+
+  def user_appriser?
+    user_signed_in? && current_user.appraiser?
+  end
+
+  def user_admin?
+    user_signed_in? && current_user.admin?
+  end
+
+  def user_secretary?
+    user_signed_in? && current_user.secretary?
+  end
+
+  def user_not_teacher?
+    user_signed_in? && !current_user.teacher?
+  end
+
+  def user_not_appriser?
+    user_signed_in? && !current_user.appraiser?
+  end
+
+  def user_not_admin?
+    user_signed_in? && !current_user.admin?
+  end
+
+  def user_not_secretary?
+    user_signed_in? && !current_user.secretary?
+  end
+
+  def user_not_teacher_not_logged?
+    user_not_teacher or !user_signed_in?
+  end
+  
 end
