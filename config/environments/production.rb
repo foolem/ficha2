2a13b21d408df6b0924f4be49865f0b73bdf2f77
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2017-10-31 13:11:44 -0200

    Configurações do servidor. Email-teste e max-threads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'ficha2.com',
+    :user_name => 'ficha2.mat@gmail.com',
+    :password => 'passwdubuntu'
+  }
+  
 end
--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,9 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 10 }.to_i
+threads_min = ENV.fetch("RAILS_MIN_THREADS") { 5 }.to_i
+threads threads_min, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
