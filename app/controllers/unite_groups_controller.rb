11ab6eb3a604c0c86bbe6152df76953a2565172f
Departamento de Matematica <dmat@MacBook-Air-de-Departamento.local>
2018-03-07 13:41:52 -0300

    new unite_groups, creating new groups

--- app/controllers/unite_groups_controller.rb
+++ app/controllers/unite_groups_controller.rb
@@ -76,65 +76,65 @@ class UniteGroupsController < ApplicationController
     if @unite_group.groups.length == 3
       groups = @unite_group.groups
       groups.each_cons(3) do |g, i, h|
-        if g.name == i.name
-          if g.name == h.name
 
-            if g.course != i.course
-              course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
-              if course.blank?
-                course = Course.create(name: "#{g.course.name}, #{i.course.name}")
-              end
-            else
-              course = g.course
-            end
-
-            g.active = false
-            g.save
-            i.active = false
-            i.save
-            h.active = false
-            h.save
-
-            group = Group.where(name: g.name, matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
-
-            if group.blank?
-              group = Group.create(name: g.name, matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}")
-            end
-
-            group.schedules = @group.schedules
-            group.save
+        if g.course.id != i.course.id && i.course.id != h.course.id && g.course.id != h.course.id
+          puts "0001"
+          course = Course.where(name: "#{g.course.name}, #{i.course.name}, #{h.course.name}").first
+          if course.blank?
+            course = Course.create(name: "#{g.course.name}, #{i.course.name}, #{h.course.name}")
           end
-        else
-          if g.course != i.course
-            if g.course != h.course
-              course = Course.where(name: "#{g.course.name}, #{i.course.name}, #{h.course.name}").first
-              if course.blank?
-                course = Course.create(name: "#{g.course.name}, #{i.course.name}, #{h.course.name}")
-              end
-            end
-          else
-            course = g.course
+        elsif g.course.id == i.course.id && i.course.id != h.course.id
+          puts "0002"
+
+          course = Course.where(name: "#{g.course.name}, #{h.course.name}").first
+          if course.blank?
+            course = Course.create(name: "#{g.course.name}, #{h.course.name}")
           end
+        elsif g.course.id == i.course.id && i.course.id == h.course.id
+          puts "0003"
 
-          g.active = false
-          g.save
-          i.active = false
-          i.save
-          h.active = false
-          h.save
-
-          group = Group.where(name: "#{g.name}, #{i.name}, #{h.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
-
-          if group.blank?
-            group = Group.create(name: "#{g.name}, #{i.name}, #{h.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}")
+          course = Course.where(name: "#{g.course.name}").first
+          if course.blank?
+            course = Course.create(name: "#{g.course.name}")
           end
+        elsif g.course.id == h.course.id && i.course.id != h.course.id
+          puts "0004"
 
-          group.schedules = @group.schedules
-          group.save
-
+          course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
+          if course.blank?
+            course = Course.create(name: "#{g.course.name}, #{i.course.name}")
+          end
         end
-        group = Group.where(name: "#{g.name}, #{i.name}", matter_id: g.matter.id).first
 
+
+        g.active = false
+        g.save
+        i.active = false
+        i.save
+        h.active = false
+        h.save
+
+        group = Group.where(name: "#{g.name}, #{i.name}, #{h.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}+#{h.vacancies}").first
+
+        if group.blank?
+          group = Group.create(name: "#{g.name}, #{i.name}, #{h.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}+#{h.vacancies}")
+        end
+
+        group.schedules = @group.schedules
+        group.save
+
+        #delete 2 group if going to 3
+
+        course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
+        puts course.name
+
+
+        group = Group.where(name: "#{g.name}, #{i.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
+        puts group
+        if group.blank?
+          group = Group.where(name: "#{i.name}, #{g.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
+        end
+        puts group
         group.destroy
 
       end
@@ -146,6 +146,9 @@ class UniteGroupsController < ApplicationController
 
   def remove
     @unite_group.groups.delete(@group)
+
+
+
     if @unite_group.groups.length == 2
       groups = @unite_group.groups
       groups.each_cons(2) do |g, i|
@@ -173,6 +176,9 @@ class UniteGroupsController < ApplicationController
 
           group.schedules = @group.schedules
           group.save
+
+
+
         else
           if g.course != i.course
             course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
@@ -197,7 +203,50 @@ class UniteGroupsController < ApplicationController
 
           group.schedules = @group.schedules
           group.save
+
         end
+        #delete if 3 groups still exists
+        course = Course.where(name: "#{g.course.name}, #{i.course.name}, #{@group.course.name}").first
+        #puts course.name
+        if course.blank?
+          course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
+        end
+        puts course.name
+
+        group = Group.where(name: "#{g.name}, #{i.name}, #{@group.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}+#{@group.vacancies}").first
+
+        if group.blank?
+          group = Group.where(name: g.name, matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}+#{@group.vacancies}").first
+        end
+
+        group.destroy
+      end
+    end
+    if @unite_group.groups.length == 1
+      groups = @unite_group.groups
+      groups << @group
+
+      groups.each_cons(2) do |g, i|
+        g.active = true
+        g.save
+        i.active = true
+        i.save
+
+        course = Course.where(name: "#{g.course.name}, #{i.course.name}").first
+        if course.blank?
+          course = Course.where(name: "#{i.course.name}, #{g.course.name}").first
+        end
+        puts course.name
+
+        group = Group.where(name: "#{g.name}, #{i.name}", matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
+        if group.blank?
+          group = Group.where(name: g.name, matter_id: g.matter.id, course_id: course.id, semester_id: Semester.current_semester.id, active: true, vacancies: "#{g.vacancies}+#{i.vacancies}").first
+        end
+        puts group
+
+        group.destroy
+        redirect_to request.referrer
+
       end
 
     end
@@ -235,38 +284,39 @@ class UniteGroupsController < ApplicationController
   def destroy
     if @unite_group.groups.length == 2
       @unite_group.groups.each_cons(2) do |a, b|
-        group_a = Group.find(a.id)
-        group_b = Group.find(b.id)
-
-        group_a.active = true
-        group_a.save
-        group_b.active = true
-        group_b.save
+        a.active = true
+        a.save
+        b.active = true
+        b.save
 
         course_united = Course.where(name: "#{a.course.name}, #{b.course.name}").first
+        if course_united.blank?
+          course_united = Course.where(name: "#{b.course.name}, #{a.course.name}").first
+          if course_united.blank?
+            course_united = Course.find(a.course.id)
+          end
+        end
         group_united = Group.where(name: "#{a.name}, #{b.name}", matter_id: a.matter.id, course_id: course_united.id).first
 
         if group_united.blank?
           group_united = Group.where(name: a.name, matter_id: a.matter.id, course_id: course_united.id).first
-          group_united.destroy
-        else
-          group_united.destroy
+          if group_united.blank?
+            group_united = Group.where(name: "#{b.name}, #{a.name}", matter_id: a.matter.id, course_id: course_united.id).first
+          end
         end
+        group_united.destroy
       end
     end
 
     if @unite_group.groups.length == 3
       @unite_group.groups.each_cons(3) do |a, b, c|
-        group_a = Group.find(a.id)
-        group_b = Group.find(b.id)
-        group_c = Group.find(c.id)
 
-        group_a.active = true
-        group_a.save
-        group_b.active = true
-        group_b.save
-        group_c.active = true
-        group_c.save
+        a.active = true
+        a.save
+        b.active = true
+        b.save
+        c.active = true
+        c.save
 
         course_united = Course.where(name: "#{a.course.name}, #{b.course.name}, #{c.course.name}").first
         group_united = Group.where(name: "#{a.name}, #{b.name}, #{c.name}", matter_id: a.matter.id, course_id: course_united.id).first
@@ -274,9 +324,8 @@ class UniteGroupsController < ApplicationController
         if group_united.blank?
           group_united = Group.where(name: a.name, matter_id: a.matter.id, course_id: course_united.id).first
           group_united.destroy
-        else
-          group_united.destroy
         end
+        group_united.destroy
       end
     end
 
